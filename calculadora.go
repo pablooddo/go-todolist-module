a97f27acf3e3c4ca08f767dfbedca41af4eb6950
pablo_oddo@hotmail.com <pablo_oddo@hotmail.com>
2022-07-13 19:56:42 -0300

    initial commit

---
+++ calculadora.go
@@ -0,0 +1,67 @@
+package todolist
+
+import "fmt"
+
+type todoList struct {
+	lista []*todoItem
+}
+
+func (tl *todoList) add(t *todoItem) {
+	tl.lista = append(tl.lista, t)
+}
+
+func (tl *todoList) remove(index int) {
+	tl.lista = append(tl.lista[:index], tl.lista[index+1:]...)
+}
+
+type todoItem struct {
+	name        string
+	description string
+	done        bool
+}
+
+func (t *todoItem) markAsDone() {
+	t.done = true
+}
+
+func show(tl todoList) {
+	fmt.Println("")
+	fmt.Println("---------------")
+	for index, t := range tl.lista {
+		fmt.Println(index, t.name, t.done)
+	}
+}
+
+/*
+func main() {
+	a := todoItem{
+		name:        "item 1",
+		description: "description of item 1",
+		done:        false,
+	}
+
+	b := &todoItem{
+		name:        "item 2",
+		description: "description of item 2",
+		done:        false,
+	}
+
+	tl := todoList{}
+
+	show(tl)
+
+	a.markAsDone()
+	tl.add(&a)
+	show(tl)
+
+	tl.add(b)
+	show(tl)
+
+	tl.remove(0)
+	show(tl)
+
+
+	todoMap := make(map[string]*todoList)
+	todoMap["PABLO"] = &tl
+}
+*/
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pablooddo/go-todolist-module
+
+go 1.18
